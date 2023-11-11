cc7df8f1987cd57928a0b80490b4428426823ff7
Davyd9V <volinskydavidbbb@gmail.com>
2023-11-11 21:21:19 +0100

    first commit

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,6 +1,10 @@
 let number1 = Math.floor(Math.random() * 100);
-if (number1 % 2 === 0) {
-    console.log(number1, ': thes is even number');
-} else {
-    console.log(number1, ': thes is odd number');
+
+function proverka() {
+    if (number1 % 2 === 0) {
+        return (number1, ': thes is even number');
+    } else {
+        return (number1, ': thes is odd number');
+    };
 };
+console.log(proverka());
