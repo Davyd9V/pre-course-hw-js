837fd5d16423b2cb9e1e23b2ac6741e6ec4b8af2
Davyd9V <volinskydavidbbb@gmail.com>
2023-11-11 20:17:18 +0100

    first commit

---
+++ lesson-02/01.js
@@ -0,0 +1,6 @@
+let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2 === 0) {
+    console.log(number1, ': thes is even number');
+} else {
+    console.log(number1, ': thes is odd number');
+};
