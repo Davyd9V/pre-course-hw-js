2798b9cd4b8a214418e5d3e58c1ff97047e1f046
Davyd9V <volinskydavidbbb@gmail.com>
2023-11-13 18:28:47 +0100

    first commit

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -2,9 +2,10 @@ let number1 = Math.floor(Math.random() * 100);
 
 function proverka() {
     if (number1 % 2 === 0) {
-        return (number1, ':Это четное число');
+        return [number1, 'Это четное число'];
     } else {
-        return (number1, ':Это нечетное число');
+        return [number1, 'Это нечетное число'];
     };
 };
-console.log(proverka());
+let result = proverka();
+console.log(`${result[0]}: ${result[1]}`);
